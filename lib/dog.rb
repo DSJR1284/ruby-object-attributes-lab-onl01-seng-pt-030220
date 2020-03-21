8882bbe519371eaa2f91ee922e84e4ded4d4dea4
Doug Jones <dsjr1284@gmail.com>
2020-03-21 01:46:54 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,12 @@
 class Dog 
   def initialize (name, breed)
-    @name= name  
-    @breed= breed 
+    @name = name  
+    @breed = breed 
   end
 
   def name= (name, breed)
-    @name= name 
-    @breed= breed 
+    @name = name 
+    @breed = breed 
   end 
   
 end 
